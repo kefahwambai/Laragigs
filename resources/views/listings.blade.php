2047cc3be45f980776793d4a662d1d777647affe
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-12-14 11:01:24 +0300

    added lisitng style and component

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,10 +1,11 @@
 @extends('layout')
 
 
-@section('content') 
+@section('content')
+@include('partials._hero') 
+@include('partials._search')
 
-
-<h1> {{$heading}} </h1>
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @if(count($listings) == 0)
     <p>No Listings found </p>
@@ -12,14 +13,9 @@
 
 
 @foreach ($listings as $listing)
-    <h2>
-     <a href="/listings/{{$listing['id']}}">
-        {{$listing['title']}}</a>
-    </h2>
-    <p>
-    {{$listing['description']}}
-    </p>
+   <x-listing-card :listing="$listing" />
 @endforeach
+</div>
 @endsection
 
     
